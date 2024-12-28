b23d887b37ad202a1a931921019e5166c7d8e670
lev1athank <nik.parh.googl@gmail.com>
2024-12-29 03:13:02 +0500

    add alerts to WIP

--- src/UI/notificationRegistrField/RegField.tsx
+++ src/UI/notificationRegistrField/RegField.tsx
@@ -11,7 +11,7 @@ import styles from "./style.module.scss";
 import { useSelector } from "react-redux";
 import { useTypeSelector } from "@/hooks/useTypeSelector";
 import { useActions } from "@/hooks/useActions";
-import z from "zod";
+import z, { map } from "zod";
 import axios, { AxiosResponse } from "axios";
 import Cookie from "js-cookie";
 import Image from "next/image";
@@ -21,6 +21,7 @@ const RegField = () => {
     const { iSregShow } = useTypeSelector((state) => state.regField);
     const { setIsRegShow } = useActions();
     const background = useRef<HTMLDivElement>(null);
+    const [alerts, setAlerts] = useState<string[]>();
 
     type a = {
         login: string;
@@ -41,16 +42,24 @@ const RegField = () => {
     }>(nullData);
 
     const data = z.object({
-        login: z.string().min(1),
-        password: z.string().min(6),
+        login: z.string().min(1, {
+            message: 'логин отсутствует'
+        }),
+        password: z.string().min(6, {
+            message: 'пароль должен быть минимум из 6 символов'
+        }),
     });
 
     const sendSubmit = async (form: FormEvent) => {
         const validation = data.safeParse(dataReg);
+        setAlerts([])
+        if (validation.error) 
+            setAlerts(validation.error.errors.map((el) => el.message));
+        if (dataReg.password !== dataReg.rePasswoed) 
+            setAlerts((prev) => (prev ? [...prev, 'Пароли не совпадают'] : ['Пароли не совпадают']))
 
-        if (validation.error) return;
-        if (dataReg.password !== dataReg.rePasswoed) return;
-
+        if(validation.error || dataReg.password !== dataReg.rePasswoed) return
+        console.log(alerts)
         try {
             const res = await axios.post("http://localhost:3452/auth/singup", {
                 ...dataReg,
@@ -82,90 +91,104 @@ const RegField = () => {
             onClick={() => {
                 setIsRegShow(false);
                 setDataReg(nullData);
+                setAlerts([])
             }}
         >
-            <div
-                className={styles.regField}
-                onClick={(e) => e.stopPropagation()}
-            >
+            <div className={styles.authField}>
                 <div
-                    className={styles.closeBtn}
-                    onClick={() => {
-                        setIsRegShow(false);
-                        setDataReg(nullData);
-                    }}
+                    className={styles.regField}
+                    onClick={(e) => e.stopPropagation()}
                 >
-                    <Image
-                        alt="close"
-                        src={"/close.svg"}
-                        width={46}
-                        height={46}
-                        className={styles.avatar}
-                    />
-                </div>
-                <form className={styles.form}>
-                    <span className={styles.titleForm}>
-                        {typeFormAuth ? "Регистрация" : "Вход"}
-                    </span>
-                    <div className={styles.inputData}>
-                        <span>
-                            <label htmlFor="login">Логин</label>
-                            <input
-                                type="text"
-                                name="login"
-                                value={dataReg.login}
-                                required={true}
-                                min={1}
-                                max={30}
-                                onChange={(e) => setData(e)}
-                            />
+                    <div
+                        className={styles.closeBtn}
+                        onClick={() => {
+                            setIsRegShow(false);
+                            setDataReg(nullData);
+                            setAlerts([])
+                        }}
+                    >
+                        <Image
+                            alt="close"
+                            src={"/close.svg"}
+                            width={46}
+                            height={46}
+                            className={styles.avatar}
+                        />
+                    </div>
+                    <form className={styles.form}>
+                        <span className={styles.titleForm}>
+                            {typeFormAuth ? "Регистрация" : "Вход"}
                         </span>
-                        <span>
-                            <label htmlFor="password">Пароль</label>
-                            <input
-                                type="password"
-                                name="password"
-                                value={dataReg.password}
-                                required={true}
-                                min={6}
-                                onChange={(e) => setData(e)}
-                            />
-                        </span>
-                        {typeFormAuth ? (
+                        <div className={styles.inputData}>
                             <span>
-                                <label htmlFor="rePasswoed">
-                                    Подтвердите пароль
-                                </label>
+                                <label htmlFor="login">Логин</label>
+                                <input
+                                    type="text"
+                                    name="login"
+                                    value={dataReg.login}
+                                    required={true}
+                                    min={1}
+                                    max={30}
+                                    onChange={(e) => setData(e)}
+                                />
+                            </span>
+                            <span>
+                                <label htmlFor="password">Пароль</label>
                                 <input
                                     type="password"
-                                    name="rePasswoed"
-                                    value={dataReg.rePasswoed}
+                                    name="password"
+                                    value={dataReg.password}
                                     required={true}
                                     min={6}
                                     onChange={(e) => setData(e)}
                                 />
                             </span>
-                        ) : (
-                            <></>
-                        )}
-                    </div>
-                    <button
-                        type="button"
-                        className={styles.btnForm}
-                        onClick={sendSubmit}
-                    >
-                        {typeFormAuth ? "Создать" : "Войти"}
-                    </button>
-                    <span
-                        className={styles.swapForm}
-                        onClick={() => {
-                            setTypeFormAuth((state) => !state);
-                            setDataReg(nullData);
-                        }}
-                    >
-                        {typeFormAuth ? "есть аккаунт" : "создать аккаунт"}
-                    </span>
-                </form>
+                            {typeFormAuth ? (
+                                <span>
+                                    <label htmlFor="rePasswoed">
+                                        Подтвердите пароль
+                                    </label>
+                                    <input
+                                        type="password"
+                                        name="rePasswoed"
+                                        value={dataReg.rePasswoed}
+                                        required={true}
+                                        min={6}
+                                        onChange={(e) => setData(e)}
+                                    />
+                                </span>
+                            ) : (
+                                <></>
+                            )}
+                        </div>
+                        <button
+                            type="button"
+                            className={styles.btnForm}
+                            onClick={sendSubmit}
+                        >
+                            {typeFormAuth ? "Создать" : "Войти"}
+                        </button>
+                        <span
+                            className={styles.swapForm}
+                            onClick={() => {
+                                setTypeFormAuth((state) => !state);
+                                setDataReg(nullData);
+                                setAlerts([])
+                            }}
+                        >
+                            {typeFormAuth ? "есть аккаунт" : "создать аккаунт"}
+                        </span>
+                    </form>
+                </div>
+                <div className={styles.alertField}>
+                    {alerts?.map((alert, index) => (
+                        <span key={index} className={styles.alert} style={{
+                            animationDelay:`${index * 0.2}s`
+                        }}>
+                            {alert}
+                        </span>
+                    ))}
+                </div>
             </div>
         </div>
     );
