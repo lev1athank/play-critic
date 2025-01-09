8b983c22b6c25338c4e4da86b9c777ba419a7adf
lev1athank <nik.parh.googl@gmail.com>
2025-01-10 01:34:42 +0500

    patch

--- src/UI/userLibrary/formNewGame/FormNewGame.tsx
+++ src/UI/userLibrary/formNewGame/FormNewGame.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
 import styles from "./style.module.scss";
+import Image from 'next/image';
 const FormNewGame = () => {
+    const [imageGame, setImageGame] = useState<string>('')
+
     return (
         <div className={styles.background}>
             <div className={styles.formNewGame}>
                 <div className={styles.gameDataField}>
-                    <div className={styles.gameImage}></div>
+                    <div className={styles.gameImage}>
+                        {
+                            imageGame ? <Image 
+                            src={imageGame}
+                            width={200}
+                            height={250}
+                            alt="game image"
+                        /> : <Image 
+                        src={"/gamepa in img.svg"}
+                        width={100}
+                        height={100}
+                        alt="game image"
+                    />
+                        }
+                        
+                    </div>  
                     <div className={styles.gameData}>
                         <div className={styles.searchGame}>
                             <span className={styles.searchTitle}>
@@ -21,6 +40,7 @@ const FormNewGame = () => {
                         <textarea className={styles.recall} maxLength={180} placeholder="Кратко раскожи свое мнение"></textarea>
                         </div>
                     </div>
+                    <div className={styles.scoreField}></div>
                 </div>
             </div>
         </div>
